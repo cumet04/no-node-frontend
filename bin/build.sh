86be36d7f515b2c0cedbd35c3b0b2be54533ae15
cumet04 <medalhkr@gmail.com>
2021-05-18 23:19:06 +0900

    fix

--- bin/build.sh
+++ bin/build.sh
@@ -3,7 +3,7 @@
 cd $(readlink -f $0 | xargs dirname)/..
 
 mkdir -p dist
-rm -r dist/*
+rm -r dist/* > /dev/null
 cp -r public/* dist/
 
-deno run --allow-read --allow-env --allow-net --allow-write --allow-run ./bin/build.ts
+deno run --allow-read --allow-env --allow-net --allow-write --allow-run ./bin/build.js
